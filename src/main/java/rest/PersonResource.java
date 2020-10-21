644aafdc835c56f49f46682d9dd4042a612d9b79
= <cph-cw109@cphbusiness.dk>
2020-10-21 12:09:44 +0200

    Nogle endpoints lavet, der mangler forskellige personDTO til de forskellige facademetode kald

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -7,11 +7,12 @@ import facades.PersonFacade;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 //Todo Remove or change relevant parts before ACTUAL use
-@Path("xxx")
+@Path("person")
 public class PersonResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
@@ -32,4 +33,46 @@ public class PersonResource {
         //System.out.println("--------------->"+count);
         return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
     }
+    
+    @Path("hobby/{hobby}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getPersonsByHobby(@PathParam("hobby") String hobby){
+        //List</*PersonDTO navn der passer til*/> personsByHobby = FACADE.getAllPersonsByHobby(hobby);
+        //return GSON.toJson(personByHobby);
+        return "";
+    }
+    
+    @Path("{city}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getPhonesByCity(@PathParam("city") String city){
+        //List</*PersonDTO navn der passer til*/> phonesByCity= FACADE.getAllPhonesByCity(city);
+        //return GSON.toJson(personByCity);
+        return "";
+    }
+    
+    @Path("hobbycount/{hobby}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getHobbyCount(@PathParam("hobby") String hobby){
+        //List</*PersonDTO navn der passer til*/> hobbyCount = FACADE.getHobbyCount(hobby);
+        //return GSON.toJson(hobbyCount);
+        return "";
+    }
+    
+    @Path("zip/{zip}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getAllZip(@PathParam("zip") String zip){
+        //List</*PersonDTO navn der passer til*/> allZip = FACADE.getAllZipCodes(zip);
+        //return GSON.toJson(allZip);
+        return "";
+    }
+    
+    
+    
+    
+    
+    
 }
