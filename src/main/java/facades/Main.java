277699866887edcbbfd7fa76fac6271dff870683
Sumit Dey <sumitdey0007@gmail.com>
2020-10-21 13:54:32 +0200

    creatperson virker

--- src/main/java/facades/Main.java
+++ src/main/java/facades/Main.java
@@ -6,28 +6,60 @@
 package facades;
 
 import dto.PersonDTO;
+import entities.Address;
+import entities.CityInfo;
 import entities.Hobby;
+import entities.Person;
 import entities.Phone;
 import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
  * @author lukas
  */
 public class Main {
-    public static void main(String[] args) {
-    PersonFacade p = new PersonFacade();
-    
-   
-    
-    ArrayList<Hobby> hobbies = new ArrayList();
-    hobbies.add(new Hobby("hashashsh", "sadasdasd"));
-    
-     ArrayList<Phone> phones = new ArrayList();
-    phones.add(new Phone("hashashsh", "sadasdasd"));
 
-    PersonDTO person = new PersonDTO("Sumit", "Dey", "sumitdey0007@gmail.com", "taastrupvej123", "taastrup", "2630", hobbies, phones);
-        System.out.println(p.createNewPerson(person));
-        
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+
+   EntityManager em = emf.createEntityManager();
+        try{
+            Person p = new Person("hans", "Andersen", "sssss");
+            Address address = new Address("ssdasdad", "asdsadsa");
+            CityInfo cityInfo = new CityInfo("2639", "hshs");
+            p.addAdress(address);
+            cityInfo.addAdress(address);
+            
+            
+            ArrayList<Hobby> hobbies = new ArrayList();
+            hobbies.add(new Hobby("sss", "sdasdsa"));
+            
+              ArrayList<Phone> phones = new ArrayList();
+            phones.add(new Phone("sadasdas", "sdasdsad"));
+            
+            
+            
+            for (Hobby hobby : hobbies) {
+                p.addHobby(hobby);
+            }
+            for(Phone phone: phones){
+                p.addPhone(phone);
+            }
+                      
+            em.getTransaction().begin();
+            em.persist(p);
+            em.persist(address);
+            em.persist(cityInfo);
+            em.getTransaction().commit();
+            
+            
+        } finally {
+            em.close();
+        }
+
     }
 }
