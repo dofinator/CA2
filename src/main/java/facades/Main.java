3742e6c4c558c783807b9b368b2e40f4f7e87fb1
Sumit Dey <sumitdey0007@gmail.com>
2020-10-21 12:04:40 +0200

    lavet createperson

--- src/main/java/facades/Main.java
+++ src/main/java/facades/Main.java
@@ -5,6 +5,11 @@
  */
 package facades;
 
+import dto.PersonDTO;
+import entities.Hobby;
+import entities.Phone;
+import java.util.ArrayList;
+
 /**
  *
  * @author lukas
@@ -12,7 +17,17 @@ package facades;
 public class Main {
     public static void main(String[] args) {
     PersonFacade p = new PersonFacade();
-        System.out.println(p.getAllPersonsByHobby("fodbold"));
+    
+   
+    
+    ArrayList<Hobby> hobbies = new ArrayList();
+    hobbies.add(new Hobby("hashashsh", "sadasdasd"));
+    
+     ArrayList<Phone> phones = new ArrayList();
+    phones.add(new Phone("hashashsh", "sadasdasd"));
 
+    PersonDTO person = new PersonDTO("Sumit", "Dey", "sumitdey0007@gmail.com", "taastrupvej123", "taastrup", "2630", hobbies, phones);
+        System.out.println(p.createNewPerson(person));
+        
     }
 }
