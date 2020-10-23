90882d54160a6068c4a956387c394dcc58f06a03
Sumit Dey <sumitdey0007@gmail.com>
2020-10-23 10:23:54 +0200

    Alle tests er lavet i ressourcetest

--- src/test/java/rest/PersonResourceTest.java
+++ src/test/java/rest/PersonResourceTest.java
@@ -1,6 +1,7 @@
 package rest;
 
 import dto.PersonDTO;
+import dto.PersonsDTO;
 import entities.Address;
 import entities.CityInfo;
 import entities.Hobby;
@@ -10,8 +11,11 @@ import exceptions.PersonNotFoundException;
 import utils.EMF_Creator;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.core.UriBuilder;
@@ -19,6 +23,8 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.util.HttpStatus;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
@@ -83,7 +89,7 @@ public class PersonResourceTest {
             p1.addAdress(a1);
             p1.addPhone(ph1);
             p2.addPhone(ph2);
-            
+
             em.persist(h1);
             em.persist(h2);
             em.persist(a1);
@@ -138,18 +144,94 @@ public class PersonResourceTest {
                 .get("/person/count").then()
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
-                //Den her skal være 2 og ikke 5
-                .body("count", equalTo(5));
+                .body("count", equalTo(2));
     }
 
     @Test
     public void testPersonByHobby() throws PersonNotFoundException {
-        PersonDTO pDTO = new PersonDTO(p1);
+
         given()
                 .contentType("application/json")
                 .get("/person/hobby/fodbold").then()
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
-                .body("fName", equalTo(pDTO.getfName()));
+                .body("size()", equalTo(1));
+
     }
+
+    @Test
+    public void testGetAllPersonsByCity() throws PersonNotFoundException {
+        given()
+                .contentType("application/json")
+                .get("/person/persons/Lyngby").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("size()", equalTo(1));
+
+    }
+
+    @Test
+    public void getPeopleCountByHobby() throws PersonNotFoundException {
+        given()
+                .contentType("application/json")
+                .get("/person/peoplecount/fodbold").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("count", equalTo(1));
+
+    }
+
+    @Test
+    public void getAllZipCodes() throws PersonNotFoundException {
+        given()
+                .contentType("application/json")
+                .get("/person/zip").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("size()", equalTo(1));
+
+    }
+
+    @Test
+    public void addPerson() throws PersonNotFoundException {
+        given()
+                .contentType(ContentType.JSON)
+                .body(new PersonDTO(p1))
+                .when()
+                .post("person/add")
+                .then()
+                .body("fName", equalTo("Sebastian"))
+                .body("lName", equalTo("Hansen"))
+                .body("email", equalTo("email@hhaa.dk"));
+
+    }
+
+    @Test
+    public void testEditPerson() {
+        Person person = new Person("Sumit", "Dey", "test@gmail.com");
+        Hobby hobby = new Hobby("fodbold", "bold med fod jalla");
+        Address address = new Address("lyngbyvej");
+        Phone phone = new Phone("1234561243", "mobil");
+        CityInfo ci = new CityInfo("2800", "Lyngby");
+        long id = 1;
+        person.setId(id);
+        person.addAdress(address);
+        person.addHobby(hobby);
+        person.addPhone(phone);
+        address.setCityInfo(ci);
+        
+        PersonDTO personDTO = new PersonDTO(person);
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(personDTO)
+                .when()
+                .put("person/edit/" + id)
+                .then()
+                .body("fName", equalTo("Sumit"))
+                .body("lName", equalTo("Dey"))
+                .body("email", equalTo("test@gmail.com"));
+
+    }
+
 }
