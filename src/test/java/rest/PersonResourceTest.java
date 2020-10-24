1aaa6b80886255197140a019f456ec17e23d15aa
Lukas Bang Stoltz-Andersen <lukasbangstoltz@gmail.com>
2020-10-24 13:12:50 +0200

    test virker nu, efter at table deletes fungere

--- src/test/java/rest/PersonResourceTest.java
+++ src/test/java/rest/PersonResourceTest.java
@@ -38,89 +38,96 @@ import org.junit.jupiter.api.Test;
 //@Disabled
 
 public class PersonResourceTest {
-    
+
     private static final int SERVER_PORT = 7777;
     private static final String SERVER_URL = "http://localhost/api";
-    
+
     private static Person p1, p2, p3;
     private static Hobby hobby1, hobby2;
     private static Address address1, address2, address3;
     private static Phone phone1, phone2, phone3;
     private static CityInfo cityInfo1, cityInfo2, cityInfo3;
-    
+
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
     private static HttpServer httpServer;
     private static EntityManagerFactory emf;
-    
+
     static HttpServer startServer() {
         ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
         return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
         //This method must be called before you request the EntityManagerFactory
         EMF_Creator.startREST_TestWithDB();
         emf = EMF_Creator.createEntityManagerFactoryForTest();
-        
+
         httpServer = startServer();
         //Setup RestAssured
         RestAssured.baseURI = SERVER_URL;
         RestAssured.port = SERVER_PORT;
         RestAssured.defaultParser = Parser.JSON;
-        
+
         EntityManager em = emf.createEntityManager();
-        
+
         p1 = new Person("Sebastian", "Hansen", "email@hhaa.dk");
         p2 = new Person("Lukas", "Bang", "yalla@habibi.dut");
         p3 = new Person("add", "add", "add@habibi.com");
-        
+
         hobby1 = new Hobby("fodbold", "bold med fod");
         hobby2 = new Hobby("håndbold", "bold med hånd");
-        
+
         address1 = new Address("lyngbyvej 22");
         address2 = new Address("fredensborgvej 244");
         address3 = new Address("hillerødvej 2223");
-        
+
         phone1 = new Phone("12345612", "mobil");
         phone2 = new Phone("65884774", "mobil");
         phone3 = new Phone("28456685", "mobil");
-        
+
         cityInfo1 = new CityInfo("2800", "Lyngby");
         cityInfo2 = new CityInfo("3480", "Fredensborg");
         cityInfo3 = new CityInfo("3400", "Hillerød");
-        
+
         address1.setCityInfo(cityInfo1);
         address2.setCityInfo(cityInfo2);
         address3.setCityInfo(cityInfo3);
-        
+
         p1.addAdress(address1);
         p1.addHobby(hobby1);
         p1.addPhone(phone1);
-        
+
         p2.addAdress(address2);
         p2.addHobby(hobby1);
         p2.addPhone(phone2);
-        
+
         try {
             em.getTransaction().begin();
-            
+
+            em.createNativeQuery("DELETE FROM PHONE").executeUpdate();
+            em.createNativeQuery("DELETE FROM HOBBY_PERSON").executeUpdate();
+            em.createNativeQuery("DELETE FROM HOBBY").executeUpdate();
+            em.createNativeQuery("DELETE FROM PERSON").executeUpdate();
+            em.createNativeQuery("DELETE FROM ADDRESS").executeUpdate();
+            em.createNativeQuery("DELETE FROM CITYINFO").executeUpdate();
+
             em.persist(address1);
             em.persist(address2);
             em.persist(address3);
             em.persist(cityInfo1);
             em.persist(cityInfo2);
             em.persist(cityInfo3);
-            
+
             em.persist(p1);
             em.persist(p2);
-            
+
             em.getTransaction().commit();
         } finally {
             em.close();
         }
     }
-    
+
     @AfterAll
     public static void closeTestServer() {
         //System.in.read();
@@ -134,9 +141,9 @@ public class PersonResourceTest {
     //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
     @BeforeEach
     public void setUp() {
-        
+
     }
-    
+
     @Test
     public void testServerIsUp() {
         System.out.println("Testing is server UP");
@@ -153,7 +160,7 @@ public class PersonResourceTest {
                 .statusCode(HttpStatus.OK_200.getStatusCode())
                 .body("msg", equalTo("Hello World"));
     }
-    
+
     @Test
     public void testCount() throws Exception {
         given()
@@ -163,19 +170,19 @@ public class PersonResourceTest {
                 .statusCode(HttpStatus.OK_200.getStatusCode())
                 .body("count", equalTo(2));
     }
-    
+
     @Test
     public void testPersonByHobby() throws PersonNotFoundException {
-        
+
         given()
                 .contentType("application/json")
                 .get("/person/hobby/fodbold").then()
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
                 .body("size()", equalTo(1));
-        
+
     }
-    
+
     @Test
     public void testGetAllPersonsByCity() throws PersonNotFoundException {
         given()
@@ -184,7 +191,7 @@ public class PersonResourceTest {
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
                 .body("size()", equalTo(1));
-        
+
     }
 //
 
@@ -196,7 +203,7 @@ public class PersonResourceTest {
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
                 .body("count", equalTo(2));
-        
+
     }
 //
 
@@ -208,9 +215,9 @@ public class PersonResourceTest {
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
                 .body("size()", equalTo(3));
-        
+
     }
-    
+
 //    @Test
 //    public void addPerson() throws PersonNotFoundException {
 //        p3.addAdress(address3);
@@ -227,7 +234,6 @@ public class PersonResourceTest {
 //                .body("email", equalTo("add@habibi.com"));
 //        
 //    }
-
 //    @Test
 //    public void testEditPerson() {
 //        Person person = new Person("Sumit", "Dey", "test@gmail.com");
